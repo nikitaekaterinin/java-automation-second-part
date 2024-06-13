a6aeffd576c9c348b8cac92864b04cf3a2a62034
Kateryna Nikitina <109725022+nikitaekaterinin@users.noreply.github.com>
2024-06-13 14:48:45 +0300

    add homework16 (#6)
    * add homework16
    
    * added fix homework16
    
    * smth
    
    ---------
    
    Co-authored-by: nikitinakateryna <kateryna.nikitina@appflame.com>

---
+++ src/main/java/org/example/ConfigFileReader.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader {
+    private static final String PROPERTY_FILE_PATH = "src/main/resources/app.properties";
+
+    private Properties properties;
+
+    public ConfigFileReader() {
+        properties = new Properties();
+        try (BufferedReader reader = new BufferedReader(new FileReader(PROPERTY_FILE_PATH))) {
+            properties.load(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getProperty(String propertyKey) {
+        var propertyValue = properties.getProperty(propertyKey);
+        if (propertyValue != null) {
+            return propertyValue;
+        }
+        throw new RuntimeException(propertyKey + " is not found");
+    }
+
+    public String getBaseUrl() {
+        return getProperty("base.url");
+    }
+}
---
+++ src/test/java/org/example/homework16/ActionIframeTests.java
@@ -0,0 +1,107 @@
+package org.example.homework16;
+
+import org.example.ConfigFileReader;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class ActionIframeTests {
+    private static final By INTERACTIONS_CARD = By.xpath("//div[@class='card-body']/h5[text()='Interactions']");
+    private static final By DROP_MENU = By.xpath("//span[@class='text' and text()='Droppable']");
+    private static final By DRAGGABLE = By.id("draggable");
+    private static final By DROPPABLE = By.id("droppable");
+    private static final By FRAMES_CARD = By.xpath("//div[@class='card-body']/h5[text()='Alerts, Frame & Windows']");
+    private static final By FRAMES_MENU = By.xpath("//span[@class='text' and text()='Nested Frames']");
+    private static final By PARENT_FRAME = By.id("frame1");
+    private static final By CHILD_FRAME = By.xpath("//body/iframe");
+    private static final By CHILD_FRAME_TEXT = By.xpath("//body/p");
+
+    WebDriver driver;
+    JavascriptExecutor js;
+
+    @Test
+    @DisplayName("Сheck element colour changing after drag&drop")
+    void checkColourChange() {
+        WebElement interactionsCard = driver.findElement(INTERACTIONS_CARD);
+        js.executeScript("arguments[0].scrollIntoView(true);", interactionsCard);
+        interactionsCard.click();
+
+        WebElement dropMenuItem = driver.findElement(DROP_MENU);
+        dropMenuItem.click();
+
+        WebElement draggable = driver.findElement(DRAGGABLE);
+        js.executeScript("arguments[0].scrollIntoView(true);", draggable);
+
+        WebElement droppable = driver.findElement(DROPPABLE);
+
+        String initialColor = droppable.getCssValue("background-color");
+
+        new Actions(driver)
+                .dragAndDrop(draggable, droppable)
+                .perform();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeToBe(droppable, "background-color", "rgba(70, 130, 180, 1)"));
+
+        String newColor = droppable.getCssValue("background-color");
+
+        assertNotEquals(initialColor, newColor);
+    }
+
+    @Test
+    @DisplayName("Check text in parent and child frames")
+    void checkFrames() {
+        WebElement framesCard = driver.findElement(FRAMES_CARD);
+        js.executeScript("arguments[0].scrollIntoView(true);", framesCard);
+        framesCard.click();
+
+        WebElement framesMenu = driver.findElement(FRAMES_MENU);
+        framesMenu.click();
+
+        WebElement parentFrame = driver.findElement(PARENT_FRAME);
+        js.executeScript("arguments[0].scrollIntoView(true);", parentFrame);
+        driver.switchTo().frame(parentFrame);
+
+        String parentFrameText = driver.findElement(By.xpath("//body")).getText();
+
+        assertEquals("Parent frame", parentFrameText);
+
+        WebElement childFrame = driver.findElement(CHILD_FRAME);
+        driver.switchTo().frame(childFrame);
+
+        WebElement childFrameText = driver.findElement(CHILD_FRAME_TEXT);
+
+        assertEquals("Child Iframe", childFrameText.getText());
+    }
+
+    @BeforeEach
+    void setup() {
+        var configs = new ConfigFileReader();
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+
+        driver.get(configs.getBaseUrl());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterEach
+    void cleanup() {
+        driver.quit();
+    }
+}
